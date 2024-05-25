7eb8e30abd95abdc5dc5c289e8e1e21cdd2b17e5
Asalek <ayoub.salek8599@gmail.com>
2024-05-25 17:47:14 +0100

    fix: comment main as it not demanded in subject

--- Dod/ex00/statistics.py
+++ Dod/ex00/statistics.py
@@ -113,13 +113,17 @@ def ft_statistics(*args: any, **kwargs: any) -> None:
             print(f"var : {var(args)}")
 
 
-def main():
-    ft_statistics(1, 42, 360, 11, 64, toto="mean", tutu="median", tata="quartile")
-    print("-----")
-    ft_statistics(5, 75, 450, 18, 597, 27474, 48575, hello="std", world="var")
-    print("-----")
-    ft_statistics(5, 75, 450, 18, 597, 27474, 48575, ejfhhe="heheh", ejdjdejn="kdekem")
-    print("-----")
-    ft_statistics(toto="mean", tutu="median", tata="quartile")
+# def main():
+#     ft_statistics(1, 42, 360, 11, 64,
+#                   toto="mean", tutu="median", tata="quartile")
+#     print("-----")
+#     ft_statistics(5, 75, 450, 18, 597, 27474, 48575,
+#                   hello="std", world="var")
+#     print("-----")
+#     ft_statistics(5, 75, 450, 18, 597, 27474, 48575,
+#                   ejfhhe="heheh", ejdjdejn="kdekem")
+#     print("-----")
+#     ft_statistics(toto="mean", tutu="median", tata="quartile")
 
-main()
+# if __name__ == "__main__":
+#     main()
